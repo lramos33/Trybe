a4ea191c484a76e3675f4786618efa88eb138588
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-29 13:32:32 -0300

    finaliza bloco 5

--- exercises/modulo-fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.4-web-storage/script.js
+++ exercises/modulo-fundamentos/bloco-5-javascript-dom-eventos-webstorage/5.4-web-storage/script.js
@@ -1,13 +1,3 @@
-/*
-As pessoas devem ter o poder de alterar:
-  Cor de fundo da tela;
-  Cor do texto;
-  Tamanho da fonte;
-  Espaçamento entre as linhas do texto;
-  Tipo da fonte ( Font family ).
-Essas preferências devem ser salvas de forma que, ao retornar à página, as preferências que foram previamente configuradas possam ser aplicadas na tela.
-*/
-
 // Constants
 const content = document.querySelector('.article');
 const btnColorBg = document.querySelectorAll('.btn-color-bg');
