49cfa2eedfabd2ae5c7d2baff457a99c47614d00
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-10-07 10:06:44 -0300

    organiza arquivos

--- Exercícios/Fundamentos/Bloco 06 - HTML e CSS forms, flexbox e responsivo/6.1 - HTML CSS forms/script.js
+++ Exercícios/Fundamentos/Bloco 06 - HTML e CSS forms, flexbox e responsivo/6.1 - HTML CSS forms/script.js
@@ -1,5 +1,5 @@
-const optionState = document.querySelector('#state');
 const stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+const optionState = document.querySelector('#state');
 
 function addStateOption() {
   for (let index = 0; index < stateOptions.length; index += 1) {
