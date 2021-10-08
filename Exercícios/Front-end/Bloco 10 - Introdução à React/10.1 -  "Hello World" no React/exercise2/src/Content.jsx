4e269129325e2d37367b4e3e461eb382e01486a0
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-10-08 11:03:07 -0300

    finaliza exercícios dia 10.1

---
+++ Exercícios/Front-end/Bloco 10 - Introdução à React/10.1 -  "Hello World" no React/exercise2/src/Content.jsx
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        {conteudos.map((content) => (
+          <div>
+            <h3>{`O conteúdo é: ${content.conteudo}`}</h3>
+            <p>{`Status: ${content.status}`}</p>
+            <p>{`Bloco: ${content.bloco}`}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Content;
