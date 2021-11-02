8b1d2e0a178974de5017591b662ad9c9e95578e4
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-11-02 15:59:08 -0300

    finaliza exercicios 12.1

--- Exercícios/Front-end/Bloco 12 - Ciclo de vida de componentes e React Router/12.1 - Ciclo de vida de componentes/exercise-dog-image/src/App.js
+++ Exercícios/Front-end/Bloco 12 - Ciclo de vida de componentes e React Router/12.1 - Ciclo de vida de componentes/exercise-dog-image/src/App.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      urlImage: '',
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  shouldComponentUpdate() {
+    localStorage.setItem('dogUrl', this.state.urlImage);
+    if (this.state.urlImage.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    const dogBreed = this.state.urlImage.split('/')[4];
+    alert(dogBreed);
+  }
+
+  fetchDog = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(response => this.setState({ 
+        urlImage: response.message,
+        loading: false,
+      }));
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    const dogSection = (
+      <div>
+        <h1>Doguinho</h1>
+        <img src={ this.state.urlImage } alt="Random dog" width='400px'/>
+        <button type="button" onClick={ this.fetchDog }>Próximo</button>
+      </div>
+    );
+    
+    return (
+      <div>
+        { loading && <h1>Loading...</h1>}
+        { !loading && dogSection }
+      </div>
+    )
+  }
 }
 
 export default App;
