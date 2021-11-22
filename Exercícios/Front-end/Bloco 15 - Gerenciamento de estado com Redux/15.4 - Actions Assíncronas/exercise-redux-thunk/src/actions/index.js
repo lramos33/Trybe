0d537f062daaef79736165197e27ae14820d7d64
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-11-22 18:49:14 -0300

    finaliza exercicio 15.4

--- Exercícios/Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.4 - Actions Assíncronas/exercise-redux-thunk/src/actions/index.js
+++ Exercícios/Front-end/Bloco 15 - Gerenciamento de estado com Redux/15.4 - Actions Assíncronas/exercise-redux-thunk/src/actions/index.js
@@ -6,5 +6,24 @@ export const requestAPI = () => ({ type: REQUEST_API });
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+  return (dispatch) => {
+    dispatch(requestAPI());
+    return fetch('https://aws.random.cat/meow')
+      .then((response) => response.json())
+      .then((data) => dispatch(getPicture(data)))
+  };
 }
+
+// Segunda opção via try/catch
+export function secondOptionFetchAPI() {
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+      const response = await fetch('https://aws.random.cat/meow');
+      const data = await response.json();
+      dispatch(getPicture(data));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
