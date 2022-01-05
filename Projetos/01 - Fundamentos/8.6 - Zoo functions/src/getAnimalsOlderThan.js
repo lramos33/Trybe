2149d39a7b7bcec8d251a0df306883f8c818d7bf
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-01-04 23:40:49 -0300

    🔧 organiza pastas do repositório

--- Projetos/01 - Fundamentos/8.6 - Zoo functions/src/getAnimalsOlderThan.js
+++
@@ -1,8 +0,0 @@
-const data = require('../data/zoo_data');
-
-function getAnimalsOlderThan(animal, age) {
-  const filteredAnimal = data.species.find((object) => object.name === animal);
-  return filteredAnimal.residents.every((object) => object.age >= age);
-}
-
-module.exports = getAnimalsOlderThan;
